b9d3f66526ee29705b85749f7334893f8a5d8891
Alex <31377763+AlexH893@users.noreply.github.com>
2025-05-31 14:48:36 -0600

    Build and deploy to GitHub Pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
+	export const prerender = true;
+
 	import '../app.css';
-	
+
 	let { children, data } = $props();
-	
 </script>
-	
-	{@render children()}
 
-
+{@render children()}
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+const dev = process.env.NODE_ENV === 'development';
 
+export default {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			// 👇 enable fallback to make this a single-page app
+			fallback: 'index.html',
+			strict: false // optional: avoids the dynamic routes warning
+		}),
+		paths: {
+			base: dev ? '' : '/search-strings'
+		}
 	}
 };
-
-export default config;
